028832ace8675a3ff505f47aa53dba9aea8750b6
vishaljoshy18 <1812-vishal@protonmail.com>
2021-07-05 14:27:28 +0530

    Adds remove button function
    gives user option to remove book from book-shelf

--- app.js
+++ app.js
@@ -22,40 +22,79 @@ function getBookReadStatus(checkbox) {
     }
 }
 
+function clearBookShelf(bookShelf) {
+    while (bookShelf.lastElementChild) {
+        bookShelf.removeChild(bookShelf.lastElementChild);
+    }
+}
+
 function displayBooks(myLibrary) {
     const bookShelf = document.querySelector('#book-shelf');
-    const bookCard = document.createElement('div');
-    bookCard.setAttribute('id', 'book-card')
-    const title = document.createElement('h1');
-    const author = document.createElement('h2');
-    const pages = document.createElement('h5');
-    const readStatus = document.createElement('button');
-    readStatus.textContent = "Read";
-    const removeButton = document.createElement('button');
-    removeButton.textContent = "Remove";
+    clearBookShelf(bookShelf);
 
 
     myLibrary.forEach(book => {
-        title.textContent = book.title;
+        const bookCard = document.createElement('div');
+        bookCard.setAttribute('id', 'book-card')
+        bookCard.setAttribute('data-bookid', myLibrary.indexOf(book));
+
+        const bookCardButtons = document.createElement('div');
+        bookCardButtons.setAttribute('id', 'book-card-buttons');
+
+        const title = document.createElement('h1');
+        const author = document.createElement('h2');
+        const pages = document.createElement('h5');
+        const readStatus = document.createElement('button');
+
+        const removeButton = document.createElement('button');
+        removeButton.setAttribute('id', 'book-remove-button');
+        removeButton.textContent = "Remove";
+
+        title.textContent = `"${book.title}"`;
         author.textContent = book.author;
         pages.textContent = book.pages;
+        if (book.readStatus == true) {
+            readStatus.textContent = "Read";
+        }
+        else {
+            readStatus.textContent = "Not Read";
+        }
+        bookCard.appendChild(title);
+        bookCard.appendChild(author);
+        bookCard.appendChild(pages);
 
+        bookCardButtons.appendChild(readStatus);
+        bookCardButtons.appendChild(removeButton);
+
+        bookCard.appendChild(bookCardButtons);
+
+        bookShelf.appendChild(bookCard);
     });
 
-    bookCard.appendChild(title);
-    bookCard.appendChild(author);
-    bookCard.appendChild(pages);
-    bookCard.appendChild(readStatus);
-    bookCard.appendChild(removeButton);
-
-    bookShelf.appendChild(bookCard);
+    onRemoveButtonClick(myLibrary);
+}
 
 
+function onRemoveButtonClick(myLibrary) {
+    const bookRemoveButton = document.querySelectorAll('#book-remove-button');
+    bookRemoveButton.forEach(button => {
+        button.addEventListener('click', () => {
+            myLibrary.splice((button.parentElement).parentElement.dataset.bookid, 1);
+            displayBooks(myLibrary);
+        })
+    });
 }
 
 function openForm() {
     document.querySelector('.form-popup').style.display = 'block';
     document.querySelector('#overlay').style.display = 'block';
+
+    window.onclick = (event) => {
+        const overlay = document.getElementById('overlay');
+        if (event.target == overlay) {
+            closeForm();
+        }
+    }
 }
 
 function closeForm() {
@@ -63,7 +102,11 @@ function closeForm() {
     document.querySelector('#overlay').style.display = 'none';
 }
 
-document.querySelector('.book-form').addEventListener('submit', (e) => {
+
+
+
+const bookForm = document.querySelector('.book-form');
+bookForm.addEventListener('submit', (e) => {
     e.preventDefault();
     const title = document.querySelector('#book-title').value;
     const author = document.querySelector('#book-author').value;
@@ -71,17 +114,12 @@ document.querySelector('.book-form').addEventListener('submit', (e) => {
     const readStatus = getBookReadStatus(document.querySelector('#book-read-status'));
 
     addBookToLibrary(title, author, pages, readStatus);
+    bookForm.reset();
     closeForm();
 });
 
 
-document.querySelector('#add-book').addEventListener('click', () => {
+document.querySelector('#add-book-button').addEventListener('click', () => {
     openForm();
 })
 
-window.onclick = function (event) {
-    let modal = document.getElementById('overlay');
-    if (event.target == modal) {
-      closeForm();
-    }
-  }
